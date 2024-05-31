44aeaa748bd74523aad83072290f24e613cb6d8f
wguadalupe <w.guadalupe78@gmail.com>
2024-05-30 22:38:41 -0400

    initial commit

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,7 @@
 // Modules and Globals
 require('dotenv').config()
+const path = require('path');
+const path = require('path');
 const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
@@ -10,7 +12,14 @@ app.use(cors())
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
 app.use(bodyParser.json())
-
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, 'client')));
+  
+    app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'client', 'index.html'));
+    });
+  }
+  
 // Controllers & Routes
 
 app.use(express.urlencoded({ extended: true }))
